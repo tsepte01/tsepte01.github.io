601d9cfa37af5020a1674207c76ad2ce51a84445
tsepte01 <tsepte01@luther.edu>
2015-10-22 10:37:00 -0500

    Update and rename Todo list.js to finaltodo.js

---
+++ finaltodo.js
@@ -0,0 +1,24 @@
+addToDo = function() {
+ ul = document.querySelector("#tdlist")
+ newli = document.createElement("li");
+ check = document.createElement("input");
+ check.type = "checkbox";
+ check.name = "done";
+ check.onclick = checkItem;
+ entry = document.querySelector("#word").value;
+ litxt = document.createTextNode(entry)
+ priority = document.querySelector("#priority").value;
+ newli.className = priority;
+ newli.appendChild(check);
+ newli.appendChild(litxt);
+ ul.appendChild(newli);
+ localSave("tdlist");
+}
+checkItem = function() {
+ if (this.checked){
+  this.parentNode.classList.add("done");
+ } else{
+  this.parentNode.classList.remove("done");
+ }
+ localSave("tdlist");
+}
